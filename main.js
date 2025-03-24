64ac47eb51bc883e3e76a77c2cb0cc78d46589c5
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2025-03-24 20:08:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2010-10-05T00:00:00"); 
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00"); 
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00"); 
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00"); 
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00"); 
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00"); 
 const tempoObjetivo4 = new Date("2025-10-05T00:00:00"); 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
